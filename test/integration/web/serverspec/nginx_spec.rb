03f30ac5408784b0fff61a8e57e9d9af2b3e0176
Craig R Webster <craig@barkingiguana.com>
2016-01-07 22:41:01 +0000

    I'd like Nginx running on the web node.

---
+++ test/integration/web/serverspec/nginx_spec.rb
@@ -0,0 +1,13 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe "Nginx" do
+  it "is listening on port 80" do
+    expect(port(80)).to be_listening
+  end
+
+  it "has a running service of nginx" do
+    expect(service("nginx")).to be_running
+  end
+end
