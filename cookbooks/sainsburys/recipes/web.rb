1131088182439fec1b1a3725241be4e20a7548b4
Craig R Webster <craig@barkingiguana.com>
2016-01-08 11:25:33 +0000

    Stop re-adding and then deleting the default site.
    Just turn it off by default for the web role.

--- cookbooks/sainsburys/recipes/web.rb
+++ cookbooks/sainsburys/recipes/web.rb
@@ -1,7 +1,3 @@
-nginx_site 'default' do
-    enable false
-end
-
 app_nodes = search :node, "role:app"
 app_ips = app_nodes.map do |node|
   node.network.interfaces.eth1.addresses.detect { |k,v| v.family == "inet" }.first
